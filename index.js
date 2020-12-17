ed5c2c3a09105a9cc06db0dbc440fe62cda8e3ec
ilyaswilian <74714631+ilyaswilian@users.noreply.github.com>
2020-12-18 06:59:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'ST4RZ BOT 🤖'; // Nama Bot Whatsapp
-const instagramlu = 'https://instagram.com/bintang_nur_pradana'; // Nama Instagramlu cok
-const whatsapplu = '0819-4698-3575'; // Nomor whatsapplu cok
+const BotName = 'Ilyaz Bot'; // Nama Bot Whatsapp
+const instagramlu = 'https://instagram.com/cc2061338sdt'; // Nama Instagramlu cok
+const whatsapplu = '0895704283153'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/FsAlnxqz6y2BhCQi5ayCLG'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/KLW3UlFfeaH36Ucm5zRfCz'; // OFFICIAL GRUP LU 2
+const grupch1 = 'https://chat.whatsapp.com/GDra1tzrztcAjglfs7VIDI'; // OFFICIAL GRUP LU 1
+const grupch2 = 'https://chat.whatsapp.com/C3iekIhPdtE2U6S7VjecTV'; // OFFICIAL GRUP LU 2
 //
 const
 {
@@ -63,7 +63,7 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by @bintang_nur_pradana`))
+conn.on('user-presence-update', json => console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by @cc2061338sdt`))
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
